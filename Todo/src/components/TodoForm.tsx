a49a14dd170f747be0905f8a634ad7c41244ead3
rahul <rahulpattusamy@gmail.com>
2025-06-08 15:24:26 +0530

    installed zod package

--- Todo/src/components/TodoForm.tsx
+++ Todo/src/components/TodoForm.tsx
@@ -1,20 +1,34 @@
 
+
+
 const TodoForm = () => {
   return (
     <form>
-        <div className="form-group mt-2  col-md-4 ">
-            <input type="text" className="form-control" placeholder="Enter the work" />
-        </div>
-         <div className="form-group mt-3 col-md-4 ">
-            <input type="text" className="form-control" placeholder="Enter the day" />
-        </div>
-         <div className="form-group mt-3  col-md-4 ">
-            <input type="text" className="form-control" placeholder="Enter the time" />
-        </div>
+      <div className="form-group mt-2  col-md-4 ">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter the work"
+        />
+      </div>
+      <div className="form-group mt-3 col-md-4 ">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter the day"
+        />
+      </div>
+      <div className="form-group mt-3  col-md-4 ">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter the time"
+        />
+      </div>
 
-        <button className=" mt-3 btn btn-primary">Submit</button>
+      <button className=" mt-3 btn btn-primary">Submit</button>
     </form>
-  )
-}
+  );
+};
 
-export default TodoForm
+export default TodoForm;
