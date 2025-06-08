dec9b1b7ad1fbfff2628600921f5dd56950bdaa8
rahul <rahulpattusamy@gmail.com>
2025-06-08 15:41:30 +0530

    applied form validation

--- Todo/src/components/TodoForm.tsx
+++ Todo/src/components/TodoForm.tsx
@@ -1,11 +1,28 @@
 
+import {intersection, z} from "zod"
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 
+
+const schema = z.object({
+  work:z.string().min(3),
+  day:z.string().min(3),
+  time:z.string().min(3)
+})
+
+type TodoData = z.infer<typeof schema>
+
+interface Props{
+  onSubmit:(data:TodoData)=>void
+}
 
 const TodoForm = () => {
+ const {register, handleSubmit} = useForm({resolver:zodResolver(schema)})
   return (
     <form>
       <div className="form-group mt-2  col-md-4 ">
         <input
+        {...register('work')}
           type="text"
           className="form-control"
           placeholder="Enter the work"
@@ -13,6 +30,7 @@ const TodoForm = () => {
       </div>
       <div className="form-group mt-3 col-md-4 ">
         <input
+        {...register('day')}
           type="text"
           className="form-control"
           placeholder="Enter the day"
@@ -20,6 +38,7 @@ const TodoForm = () => {
       </div>
       <div className="form-group mt-3  col-md-4 ">
         <input
+        {...register('time')}
           type="text"
           className="form-control"
           placeholder="Enter the time"
